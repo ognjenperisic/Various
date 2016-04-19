a517611c5ca13c1a51c7751322edaace7b4d6b12
Ognjen Perišić <ognjenperisic@users.noreply.github.com>
2016-04-19 09:24:01 +0200

    Update extract_residues_1.py

--- extract_residues_1.py
+++ extract_residues_1.py
@@ -12,7 +12,7 @@ chains = []
 
 f_in  = open(sys.argv[1], 'r')
 for line in f_in:  
-   elements = line.split()
+   elements = line.split() # this line is not correct, using 'split' is not correct
    if elements[0]=='ATOM' and elements[2]=='CA':
    #if elements[0]=='ATOM':
 
